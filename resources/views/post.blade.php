c889673eb02db7d8ba9ce8d410129d2c9d33a14e
roch49 <rochellemartins25@gmail.com>
2021-06-07 14:47:24 +0530

    Get posts from file system

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1> {{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}   
+        </div>
+    </article>    
+@endsection
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="posts/{{ $post->slug }}">
+                {{ $post->title }} 
+            </a>
+        </h1>
+        <div>
+            {{ $post->except }}
+        </div>
+    </article>
+    @endforeach    
+@endsection
+
+
